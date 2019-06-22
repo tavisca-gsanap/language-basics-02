9f3e93494946e3d66b50b49c6aead8531ebabe98
Govind Sanap <gsanap@tavisca.in>
2019-06-22 12:22:30 +0530

    Written program.

--- Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise2/Program.cs
@@ -24,7 +24,54 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
         public static string GetCurrentTime(string[] exactPostTime, string[] showPostTime)
         {
             // Add your code here.
-            throw new NotImplementedException();
+            string ans;
+            DateTime[] dateValue = new DateTime[exactPostTime.Length];
+            int h=0, m=0, s=0;
+            bool f = true;
+            DateTime d;
+            for (int i = 0; i < exactPostTime.Length; i++)
+            {
+                if (DateTime.TryParse(exactPostTime[i], out dateValue[i]))
+                {
+                    string[] words = showPostTime[i].Split(' ');
+                    if (f)
+                    {
+                        //will be executed only once.
+                        h = dateValue[i].Hour;
+                        m = dateValue[i].Minute;
+                        s = dateValue[i].Second;
+                        f = false;
+                    }
+                    else
+                    {
+                        //we will check wheather repeated time is given.
+                        if (dateValue[i-1].ToString("HH:mm:ss").Equals(dateValue[i].ToString("HH:mm:ss")) && (!(showPostTime[i-1].Split(' ')[0].Equals(words[0])) || !(showPostTime[i-1].Split(' ')[1].Equals(words[1]))))
+                        {
+                            return "impossible";
+                        }
+                    }
+                    if (words[1].Equals("hours"))
+                    {
+                        h = dateValue[i].AddHours(int.Parse(words[0])).Hour;
+                    }
+                    else if (words[1].Equals("minutes"))
+                    {
+                        d = dateValue[i].AddMinutes(int.Parse(words[0]));
+                        h = d.Hour;
+                        m = d.Minute;
+                        s = dateValue[i].Second;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Unable to convert '{0}' to a date.", exactPostTime[i]);
+                }
+            }
+            string h1 = ""+h;
+            if (h == 0) h1 = "00";
+            ans = h1 + ":" + m;
+            ans += ":" + s;
+            return ans;
         }
     }
 }
